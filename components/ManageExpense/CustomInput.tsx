9bc364eabd7c65731e0256b1d648577f9c0b27b0
Miana Neagos <miananeagos@gmail.com>
2025-03-11 15:42:20 +0200

    feat: enhance CustomInput and ExpenseForm components with improved styling and functionality

--- components/ManageExpense/CustomInput.tsx
+++ components/ManageExpense/CustomInput.tsx
@@ -1,17 +1,40 @@
-import { View, TextInput, Text, KeyboardTypeOptions, TextInputProps } from "react-native";
+import { View, TextInput, Text, TextInputProps, StyleSheet, StyleProp, ViewStyle, TextStyle } from "react-native";
+import { globalStyles } from "../../constants/styles";
 
 type InputProps = {
     label : string,
     textInputConfig? : TextInputProps;
+    style? : StyleProp<ViewStyle | TextStyle>
 }
 
-const CustomInput = ({label, textInputConfig} : InputProps) => {
+const CustomInput = ({label, style,  textInputConfig} : InputProps) => {
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...textInputConfig}/>
+        <View style={[styles.inputContainer, style]}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={styles.input} {...textInputConfig}/>
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: 'center',
+        marginHorizontal: 20,
+    },
+    label: {
+        marginBottom: 4,
+        marginTop: 8,
+        color: globalStyles.colors.primary50,
+        fontFamily: 'Roboto_700Bold',
+    },
+    input: {
+        backgroundColor: globalStyles.colors.primary50,
+        paddingHorizontal: 5,
+        paddingVertical: 6,
+        borderRadius: 5,
+        fontSize: 18,
+        fontFamily: 'Roboto_400Regular',
+        textAlignVertical: 'top',
+    },
+})
 export default CustomInput;
+
--- components/ManageExpense/ExpenseForm.tsx
+++ components/ManageExpense/ExpenseForm.tsx
@@ -1,35 +1,67 @@
-import { View } from "react-native"
-import CustomInput from "./CustomInput"
+import { View, StyleSheet, Text } from "react-native";
+import CustomInput from "./CustomInput";
+import { globalStyles } from "../../constants/styles";
 
 export const ExpenseForm = () => {
+  const amountInputHandler = (input: string) => {
+    console.log("Amount input handler", input);
+  };
+  return (
+    <View style={styles.formContainer}>
+      <Text style={styles.title}>Your new expense:</Text>
+      <View style={styles.dateAmountContainer}>
+        <CustomInput
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountInputHandler,
+          }}
+          style={styles.rowInput}
+        />
+        <CustomInput
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            keyboardType: "number-pad",
+            onChangeText: (input) => {
+              console.log("Date input handler", input);
+            },
+          }}
+          style={styles.rowInput}
+        />
+      </View>
+      <CustomInput
+        label="Description"
+        textInputConfig={{
+          // placeholder: 'Enter description',
+          multiline: true,
+          autoCorrect: false,
+          onChangeText: (input) => {
+            console.log("Description input handler", input);
+          },
+        }}
+      />
+    </View>
+  );
+};
 
-    const amountInputHandler = (input : string) => {
-        console.log("Amount input handler", input)
-    }
-    return (
-        <View>
-            <CustomInput  label="Amout" textInputConfig={{
-                keyboardType: "decimal-pad",
-                onChangeText: amountInputHandler,
-            }}/>
-            <CustomInput  label="Date" textInputConfig={{
-                placeholder: 'YYYY-MM-DD',
-                maxLength: 10,
-                keyboardType: "number-pad",
-                onChangeText: (input) => {
-                    console.log("Date input handler", input);
-                    
-                },
-            }}/>
-            <CustomInput  label="Description" textInputConfig={{
-                placeholder: 'Enter description',
-                multiline: true,
-                autoCorrect: false, 
-                onChangeText: (input) => {
-                    console.log("Description input handler", input);
-                    
-                },
-            }}/>     
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  formContainer: {
+    marginTop: 35,
+    marginBottom: 35,
+  },
+  title: {
+    marginHorizontal: 20,
+    fontFamily: "Roboto_700Bold",
+    color: globalStyles.colors.primary50,
+    fontSize: 18,
+  },
+  dateAmountContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
